d9831c17694cc29598c3466d895721a5eac3a4ee
eagle-games <74966122+Eagle-games@users.noreply.github.com>
2022-06-18 21:34:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import * as THREE from'https://unpkg.com/three@0.126.1/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://unpkg.com/three@0.126.1/examples/jsm/loaders/GLTFLoader.js';
-import "./index.css";
 
 let camera;
 let scene;
