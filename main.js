006118ff0a9a2866cd7b0746dbf812c1c9774d18
eagle-games <74966122+Eagle-games@users.noreply.github.com>
2022-06-18 22:37:32 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 import sheet from 'https://eagle-games.github.io/nextnonetypeschool/style.css' assert { type: 'css' };
-document.adoptedStyleSheets = [sheet];
-shadowRoot.adoptedStyleSheets = [sheet];
 import * as THREE from'https://unpkg.com/three@0.126.1/build/three.module.js';
 
 // Canvas
