b8d40121afa7ae32a256af61fb5356856e7bff9f
eagle-games <74966122+Eagle-games@users.noreply.github.com>
2022-06-18 23:02:35 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import sheet from './styles.css';
+import sheet from 'https://eagle-games.github.io/nextnonetypeschool/styles.css';
 import * as THREE from'https://unpkg.com/three@0.126.1/build/three.module.js';
 
 // Canvas
